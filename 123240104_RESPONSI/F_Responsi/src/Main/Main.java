9bc50936dc9160c4dd7674aa508741ce15fb55ce
MassJakaa <rafidzaka5@gmail.com>
2026-05-20 12:33:34 +0700

    Add files via upload

---
+++ 123240104_RESPONSI/F_Responsi/src/Main/Main.java
@@ -0,0 +1,11 @@
+//NAMA= Rafi Dzaka Pratama Putra
+//NIM= 123240104
+//KELAS= IF - F
+package Main;
+import View.Karyawan.ViewData;
+
+public class Main {
+    public static void main(String[] args) {
+        new ViewData();
+    }
+}
